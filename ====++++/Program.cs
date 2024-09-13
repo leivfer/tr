37857b400c381460ebea6c72947ffee7b9534486
Stephan <besogonovstepa2006@yandex.ru>
2024-09-13 15:34:06 +0400

    Add files via upload

---
+++ ====++++/Program.cs
@@ -0,0 +1,8 @@
+﻿while (true)
+{ 
+Console.WriteLine("Введи число!");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введи число!");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine(a + b);
+}
